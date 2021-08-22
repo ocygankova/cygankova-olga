430d210d5b85d443f9a2ac38913995e5d1512d9b
Olga Cygankova <olga.cygankowa@gmail.com>
2021-08-22 22:14:11 +0200

    homework-lesson-22

---
+++ homework/lesson-22/js/scripts.js
@@ -0,0 +1,16 @@
+
+let ab = 3;
+let bc = 7;
+let sR = ab * bc;
+console.log('Площадь прямоугольника:' + ' ' + sR);
+
+const PI = 3.1415;
+let r = 10;
+let sC = PI * r ** 2;
+console.log('Площадь круга:' + ' ' + sC);
+
+let a = 55;
+let n = 3;
+let aN = a ** n;
+console.log('Степень числа:' + ' ' + aN);
+
