f04bef1199e63151c57ffacd873b1d17dd74d246
Olga Cygankova <olga.cygankowa@gmail.com>
2021-08-30 11:14:22 +0200

    homework-lesson24

---
+++ homework/lesson-24/script.js
@@ -0,0 +1,13 @@
+function f1() {
+    document.getElementById('out1').textContent =
+        document.getElementById('name').value;
+    document.getElementById('out2').textContent =
+        document.getElementById('surname').value;
+    document.getElementById('out3').textContent =
+        document.getElementById('age').value;
+    document.getElementById('out4').textContent =
+        document.getElementById('city').value;
+    document.getElementById('out5').textContent =
+        document.getElementById('job').value;
+}
+document.getElementById('send').onclick = f1;
