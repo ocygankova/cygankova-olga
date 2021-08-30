81817af8e299b2d0e715bb998613605498bd5c94
Olga Cygankova <olga.cygankowa@gmail.com>
2021-08-30 13:09:03 +0200

    clear input after submit

--- homework/lesson-24/script.js
+++ homework/lesson-24/script.js
@@ -1,13 +1,17 @@
 function f1() {
-    document.getElementById('out1').textContent =
-        document.getElementById('name').value;
-    document.getElementById('out2').textContent =
-        document.getElementById('surname').value;
-    document.getElementById('out3').textContent =
-        document.getElementById('age').value;
-    document.getElementById('out4').textContent =
-        document.getElementById('city').value;
-    document.getElementById('out5').textContent =
-        document.getElementById('job').value;
+    document.getElementById('out1').textContent = document.getElementById('name').value;
+    document.getElementById('name').value = '';
+
+    document.getElementById('out2').textContent = document.getElementById('surname').value;
+    document.getElementById('surname').value = '';
+
+    document.getElementById('out3').textContent = document.getElementById('age').value;
+    document.getElementById('age').value = '';
+
+    document.getElementById('out4').textContent = document.getElementById('city').value;
+    document.getElementById('city').value = '';
+
+    document.getElementById('out5').textContent = document.getElementById('job').value;
+    document.getElementById('job').value = '';
 }
 document.getElementById('send').onclick = f1;
